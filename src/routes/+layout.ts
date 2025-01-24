4a2085490300e867aaafdbdcc2a23233e0b27472
EMPTYVOID-DEV <keskasaymen8@gmail.com>
2025-01-24 21:37:40 +0100

    1-adding shadcn 2-adding i18n setup with typesafe-i18n

---
+++ src/routes/+layout.ts
@@ -0,0 +1,19 @@
+import { browser } from '$app/environment';
+import type { Load } from '@sveltejs/kit';
+import { detectLocale } from '$lib/client/i18n/i18n-util';
+import { loadLocale } from '$lib/client/i18n/i18n-util.sync';
+import { setLocale } from '$lib/client/i18n/i18n-svelte';
+import { detectLangDirection } from '$lib/client/utils';
+import cookies from 'browser-cookies';
+
+export const load: Load = async () => {
+	if (browser) {
+		const lang = cookies.get('lang') || '';
+		const locale = detectLocale(() => [lang]);
+		const direction = detectLangDirection(locale);
+		document.documentElement.setAttribute('dir', direction);
+		document.documentElement.setAttribute('lang', locale);
+		loadLocale(locale);
+		setLocale(locale);
+	}
+};
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,11 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script lang="ts">
+	import LL from '$lib/client/i18n/i18n-svelte';
+	import { setLang } from '$lib/client/utils';
+	import { Button } from '@components/shadcn/button';
+</script>
+
+<div class="flex h-svh w-svw flex-col items-center justify-center gap-1">
+	<h1 class="text-primary">{$LL.title()}</h1>
+	<p class="text-foreground">{$LL.intro()}</p>
+	<Button onclick={() => setLang('ar')}>{$LL.startNow()}</Button>
+</div>
