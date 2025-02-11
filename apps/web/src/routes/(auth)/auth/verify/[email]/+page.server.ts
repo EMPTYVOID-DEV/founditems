dc80be8147984df76073f791005875858bc77d55
EMPTYVOID-DEV <keskasaymen8@gmail.com>
2025-02-11 15:07:15 +0100

    1-turning the project into a pnpm workspace 2-extracting the db stuff to their own package 3-extract mail stuff to their own package 4-adjusting the web app to use the packages

---
+++ apps/web/src/routes/(auth)/auth/verify/[email]/+page.server.ts
@@ -0,0 +1,40 @@
+import { db, userTable, eq } from 'db';
+import { createSessionWrapper } from '@server/utils/auth';
+import { isValidOtp, setupOtp } from 'mail';
+import { profilePage } from '@shared/const';
+import { LL } from '@assets/i18n/i18n';
+import { getValidator, getEmailSchema } from '@shared/zod';
+import { fail, type Actions, redirect } from '@sveltejs/kit';
+
+export const actions: Actions = {
+	resend: async ({ params }) => {
+		const email = params.email!;
+		const emailValidated = getValidator(getEmailSchema())(email);
+		if (emailValidated.status == 'invalid') return fail(400, { message: emailValidated.errorMsg });
+		await setupOtp(email);
+	},
+	verify: async ({ cookies, request, params }) => {
+		const fd = await request.formData();
+		const email = params.email!;
+		const enteredOtp = fd.get('otp')?.toString() || '';
+		const emailValidated = getValidator(getEmailSchema())(email);
+
+		if (emailValidated.status == 'invalid') return fail(400, { message: emailValidated.errorMsg });
+
+		const isValid = await isValidOtp(email, enteredOtp);
+
+		if (isValid == 'invalid') return fail(403, { message: LL.auth.incorrectCode() });
+
+		if (isValid == 'expired') return fail(403, { message: LL.auth.expiredCode() });
+
+		const userInfo = (
+			await db
+				.update(userTable)
+				.set({ verified: true })
+				.where(eq(userTable.email, email))
+				.returning({ id: userTable.id })
+		)[0];
+		await createSessionWrapper(cookies, userInfo.id);
+		redirect(303, profilePage);
+	}
+};
