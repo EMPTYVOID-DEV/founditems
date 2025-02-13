717c1779cb028d1e9ebe3c7929f079afe8e4ff1c
EMPTYVOID-DEV <keskasaymen8@gmail.com>
2025-02-13 17:16:51 +0100

    fixing a type inference error in matcher by upgrading hono,adding prettier and eslint

--- packages/db/tsup.config.ts
+++ packages/db/tsup.config.ts
@@ -1,15 +1,15 @@
-import { defineConfig } from "tsup";
+import { defineConfig } from 'tsup';
 
 export default defineConfig(() => ({
-  entry: ["./src/index.ts"],
-  dts: true,
-  outDir: "dist",
-  format: ["esm"],
-  name: "bundle",
-  splitting: false,
-  sourcemap: true,
-  target: "esnext",
-  clean: true,
-  minify: true,
-  metafile: false,
+	entry: ['./src/index.ts'],
+	dts: true,
+	outDir: 'dist',
+	format: ['esm'],
+	name: 'bundle',
+	splitting: false,
+	sourcemap: true,
+	target: 'esnext',
+	clean: true,
+	minify: true,
+	metafile: false
 }));
